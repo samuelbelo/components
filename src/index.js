90a2ba73a10766ebf894284ea74e5b60ec8ad619
Samuel <sam-uka-54@hotmail.com>
2019-05-20 15:13:36 -0300

    identation

--- src/index.js
+++ src/index.js
@@ -9,29 +9,29 @@ const App = () => {
     return (
         <div className="ui container comments">
             <ApprovalCard>
-            <CommentDetail 
-            author="Sam" 
-            timeAgo="Today at 4:45" 
-            content="Excelent Work!" 
-            avatar={faker.image.avatar()}
-            />
+                <CommentDetail 
+                    author="Sam" 
+                    timeAgo="Today at 4:45" 
+                    content="Excelent Work!" 
+                    avatar={faker.image.avatar()}
+                />
             </ApprovalCard>
             
             <ApprovalCard>
-            <CommentDetail 
-            author="Alex" 
-            timeAgo="Today at 4:42" 
-            content="Nice post!"
-            avatar={faker.image.avatar()}
-            />
+                <CommentDetail 
+                    author="Alex" 
+                    timeAgo="Today at 4:42" 
+                    content="Nice post!"
+                    avatar={faker.image.avatar()}
+                />
             </ApprovalCard>
             <ApprovalCard>
-            <CommentDetail
-            author="Jane " 
-            timeAgo="Yersterday at 4:45" 
-            content="Keep up with tha good work bruh"
-            avatar={faker.image.avatar()}
-            />
+                <CommentDetail
+                    author="Jane " 
+                    timeAgo="Yersterday at 4:45" 
+                    content="Keep up with tha good work bruh"
+                    avatar={faker.image.avatar()}
+                />
             </ApprovalCard>
         </div>
     );
