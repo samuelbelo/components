5c906b4642aa1917b991cc4aba01e990761bf545
Samuel <sam-uka-54@hotmail.com>
2019-05-20 15:09:53 -0300

    nesting more components

--- src/index.js
+++ src/index.js
@@ -8,27 +8,31 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
     return (
         <div className="ui container comments">
-            <ApprovalCard />
+            <ApprovalCard>
             <CommentDetail 
             author="Sam" 
             timeAgo="Today at 4:45" 
             content="Excelent Work!" 
             avatar={faker.image.avatar()}
             />
+            </ApprovalCard>
             
+            <ApprovalCard>
             <CommentDetail 
             author="Alex" 
             timeAgo="Today at 4:42" 
             content="Nice post!"
             avatar={faker.image.avatar()}
             />
-            
-            <CommentDetail 
+            </ApprovalCard>
+            <ApprovalCard>
+            <CommentDetail
             author="Jane " 
             timeAgo="Yersterday at 4:45" 
             content="Keep up with tha good work bruh"
             avatar={faker.image.avatar()}
             />
+            </ApprovalCard>
         </div>
     );
 ;}
